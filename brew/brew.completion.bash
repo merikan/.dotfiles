82ee156f72f8b0782b937833aa19c448b68879a8
Peter Merikan <peter@merikan.com>
2016-03-13 20:52:49 +0100

    clean up bash and brew completion

--- brew/brew.completion.bash
+++ brew/brew.completion.bash
@@ -1,8 +1,6 @@
-if which brew >/dev/null 2>&1; then
-  if [ -f `brew --prefix`/etc/bash_completion ]; then
-    . `brew --prefix`/etc/bash_completion
-  fi
-
+# Completion for brew command is installed but not turned on by default,
+# so we have to give it a little help
+if command -v brew >/dev/null 2>&1; then
   if [ -f `brew --prefix`/Library/Contributions/brew_bash_completion.sh ]; then
     . `brew --prefix`/Library/Contributions/brew_bash_completion.sh
   fi
