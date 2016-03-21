fe6683fa0fda1020dc55e24ee4745b50d70cf826
Peter Merikan <peter@merikan.com>
2016-03-21 11:19:01 +0100

    hmebrew: Fix most of bash completion problems
    Didn't load bash completion files installed in etc/bash-completion.d by formulea.
    Still have problem with cd command when installing bash-completion formula.

--- brew/bash.completion.bash
+++
@@ -1,6 +0,0 @@
-# Turn on bash-completion if it's installed
-if command -v brew >/dev/null 2>&1; then
-  if [ -f `brew --prefix`/etc/bash_completion ]; then
-    . `brew --prefix`/etc/bash_completion
-  fi
-fi
--- brew/brew.completion.bash
+++ brew/brew.completion.bash
@@ -1,7 +1,23 @@
-# Completion for brew command is installed but not turned on by default,
-# so we have to give it a little help
+# Source completion files for homebrew
+
 if command -v brew >/dev/null 2>&1; then
-  if [ -f `brew --prefix`/Library/Contributions/brew_bash_completion.sh ]; then
-    . `brew --prefix`/Library/Contributions/brew_bash_completion.sh
+  # Turn on bash-completion if it's installed
+  if [ -s `brew --prefix`/etc/bash_completion ]; then
+    source `brew --prefix`/etc/bash_completion
+  else
+    # Completion file for the brew command itself is installed but not turned on 
+    # by default, so we have to give it a little help. 
+    # FYI: If formula bash-completion is installed it will copy this file 
+    # to `brew --prefix`/etc/bash_completion.d. Yes COPY!!
+    # directory. 
+    if [ -s `brew --prefix`/Library/Contributions/brew_bash_completion.sh ]; then
+      source `brew --prefix`/Library/Contributions/brew_bash_completion.sh
+    fi 
+    
+    # Most formulas put their completion files in the `brew --prefix`/etc/bash_completion.d 
+    # directory. We have to source them ourseleves if formula bash-completion isn't installed
+    if [ -d `brew --prefix`/etc/bash_completion.d ]; then
+      for f in `brew --prefix`/etc/bash_completion.d/*; do source $f; done
+    fi
   fi
 fi
