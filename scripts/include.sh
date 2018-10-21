94f9a27160b7cfac998fafc6f47ecdd82cd7d27c
Peter Merikan <peter@merikan.com>
2018-10-21 17:13:00 +0200

    homebrew: add confirmation dialog to brewit

--- scripts/include.sh
+++ scripts/include.sh
@@ -3,9 +3,9 @@
 ### --------------------------------- ###
 #    Handle errors
 ### --------------------------------- ###
-# # exit on errors
-set -o nounset # exit on use of uninitialized variable
-set -o errtrace # inherits trap on ERR in function and subshell
+# to use this, copy to your script
+#set -o nounset # exit on use of uninitialized variable
+#set -o errtrace # inherits trap on ERR in function and subshell
 
 trap 'traperror $? $LINENO $BASH_LINENO "$BASH_COMMAND" $(printf "::%s" ${FUNCNAME[@]:-})' ERR
 #trap 'trapexit $? $LINENO' EXIT
@@ -20,46 +20,52 @@ function traperror () {
     local linecallfunc=$3
     local command="$4"
     local funcstack="$5"
-    fail "Command '$command' failed at line $line - exited with status: $err" 
 
     if [ "$funcstack" != "::" ]; then
       if [ "$linecallfunc" != "" ]; then
         local funcErr="called at line $linecallfunc"
       fi
-      echo -n "$(date) $(hostname) $0: DEBUG Error in ${funcstack} $funcErr"
+      error "$(date) $(hostname) $0: DEBUG Error in ${funcstack} $funcErr"
     fi
-    echo "'$command' failed at line $line - exited with status: $err"
+    fail "Command '$command' failed at line $line - exited with status: $err" 
 }
 
 ### --------------------------------- ###
 #    Print messages
 ### --------------------------------- ###
-reset='\e[0m'           # Text Reset
-red='\e[0;31m'          # Red
-green='\e[0;32m'        # Green
-yellow='\e[0;33m'       # Yellow
-blue='\e[0;34m'         # Blue
-purple='\e[0;35m'       # Purple
-cyan='\e[0;36m'         # Cyan
-white='\e[0;37m'        # White
+reset='\033[0m'           # Text Reset
+red='\033[0;31m'          # Red
+green='\033[0;32m'        # Green
+yellow='\033[0;33m'       # Yellow
+blue='\033[0;34m'         # Blue
+purple='\033[0;35m'       # Purple
+cyan='\033[0;36m'         # Cyan
+white='\033[0;37m'        # White
 
+println() {
+    printf -- "${1:-}\n"
+}
 heading() {
-    printf "\n${white}${1:-}${reset}\n"
+    printf -- "\n${white}${1:-}${reset}\n"
 }
 
 info() {
-    printf "[ ${blue}..${reset} ] ${1:-}\n"
+    printf -- "[ ${blue}..${reset} ] ${1:-}\n"
 }
 
 success() {
-    printf "[ ${green}OK${reset} ] ${1:-}\n"
+    printf -- "[ ${green}OK${reset} ] ${1:-}\n"
 }
 
 warn() {
-    printf "[${yellow}WARN${reset}] ${1:-}\n"
+    printf -- "[${yellow}WARN${reset}] ${1:-}\n"
+}
+
+error() {
+    printf -- "[${red}FAIL${reset}] ${1:-}\n"
 }
 
 fail() {
-    printf "[${red}FAIL${reset}] ${1:-}\n"
+    error ${1:-}
     exit 1
 }
