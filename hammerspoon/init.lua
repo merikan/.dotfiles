f70557a7d6c5a28a87cda285fb5819c493187897
Peter Merikan <peter@merikan.com>
2020-07-08 20:23:23 +0200

    add my missing hammerspoon config

---
+++ hammerspoon/init.lua
@@ -0,0 +1,82 @@
+--- ==============================================================================  ---
+--- Configuration
+--- ==============================================================================  ---
+-- load local file
+localfile = hs.configdir .. "/init.local.lua"
+if hs.fs.attributes(localfile) then
+  dofile(localfile)
+end
+
+hs.logger.defaultLogLevel="info"
+hs.window.animationDuration = 0
+
+hs.loadSpoon("SpoonInstall")
+spoon.SpoonInstall.use_syncinstall = true
+Install=spoon.SpoonInstall
+
+
+-- key mappings
+local hyper = { "ctrl", "alt", "cmd" }
+local shift_hyper = { "shift", "ctrl", "alt", "cmd" }
+
+-- show hotkeys
+hs.hotkey.showHotkeys(hyper,"s")
+-- toggle console
+hs.hotkey.bindSpec({ hyper, "y" }, hs.toggleConsole)
+-- reload config
+hs.hotkey.bindSpec({ hyper, "r" }, hs.reload)
+
+
+--- ==============================================================================  ---
+--- Spoons
+--- ==============================================================================  ---
+hs.loadSpoon("SpoonInstall")
+Install=spoon.SpoonInstall -- abbreviation for easier use
+Install.use_syncinstall = true -- use sync notifications
+-- my personal spoon repository
+spoon.SpoonInstall.repos.merikan = {
+  url = "https://github.com/merikan/Spoons",
+  desc = "my personal spoon repository",
+}
+
+
+-- http://www.hammerspoon.org/Spoons/ReloadConfiguration.html
+Install:andUse("ReloadConfiguration", {
+  start = true,
+  hotkeys = {
+    reloadConfiguration = { hyper, "r" }
+  },
+})
+
+-- http://www.hammerspoon.org/Spoons/WindowGrid.html
+Install:andUse("WindowGrid", {
+                  disabled = true,
+                  start = true,
+                  config = {gridGeometries = { { "6x4" } } },
+                  hotkeys = {show_grid = { hyper, "g" } },
+})
+
+-- -- http://www.hammerspoon.org/Spoons/WindowHalfsAndThirds.html
+-- Install:andUse("WindowHalfsAndThirds", 
+-- {
+--                   disabled = true,
+--                   hotkeys = 'default'
+-- })
+
+-- http://www.hammerspoon.org/Spoons/MiroWindowsManager.html
+Install:andUse("MiroWindowsManager",
+                {
+                  hotkeys = {
+                    left =       { hyper, "left" },
+                    up =         { hyper, "up" },
+                    down =       { hyper, "down" },
+                    right =      { hyper, "right" },
+                    fullscreen = { hyper, "f" }
+                  }
+                }
+)
+
+
+
+
+hs.notify.show("Hammerspoon", "Configuration (re)loaded", '')
