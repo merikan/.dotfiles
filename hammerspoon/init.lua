b4664a4ba83d1486843e8af066941965d64ef3d8
Peter Merikan <peter@merikan.com>
2021-03-19 07:49:02 +0100

    hs: add function to move window to display

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -26,6 +26,17 @@ hs.hotkey.bindSpec({ hyper, "y" }, hs.toggleConsole)
 -- reload config
 hs.hotkey.bindSpec({ hyper, "r" }, hs.reload)
 
+local function moveWindowToDisplay(d)
+  return function()
+    local displays = hs.screen.allScreens()
+    local win = hs.window.focusedWindow()
+    win:moveToScreen(displays[d], false, true)
+  end
+end
+
+hs.hotkey.bind({"ctrl", "alt", "cmd"}, "1", moveWindowToDisplay(1))
+hs.hotkey.bind({"ctrl", "alt", "cmd"}, "2", moveWindowToDisplay(2))
+hs.hotkey.bind({"ctrl", "alt", "cmd"}, "3", moveWindowToDisplay(3))
 
 --- ==============================================================================  ---
 --- Spoons
