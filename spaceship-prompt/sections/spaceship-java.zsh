c16757bf0e18af86a55eb9ba702383865bf10b44
Peter Merikan <peter@merikan.com>
2021-04-10 11:57:17 +0200

    spaceship-prompt: check for another gradle file in java section

--- spaceship-prompt/sections/spaceship-java.zsh
+++ spaceship-prompt/sections/spaceship-java.zsh
@@ -20,18 +20,19 @@ spaceship_java() {
 
   spaceship::exists java || return
 
-  # Show Java version only in java context
+  # Show Java version only in Java context
   if [[ $SPACESHIP_JAVA_CONTEXT_ONLY == true ]]; then
     local is_in_context=false
     
-    if [[ -f pom.xml || -f build.gradle || -n *.(java|jar|war|class)(#qN^/) ]]; then
+    if [[ -f pom.xml || -f build.gradle || -f settings.gradle || 
+          -n *.(java|jar|war|class)(#qN^/) ]]; then
       is_in_context=true
     elif spaceship::is_git; then # java project?
       local git_root=$(git rev-parse --show-toplevel)
       if (cygpath --version) >/dev/null 2>/dev/null; then
         git_root=$(cygpath -u $git_root)
       fi
-      if [[ -f $git_root/pom.xml || -f $git_root/build.gradle ]]; then
+      if [[ -f $git_root/pom.xml || -f $git_root/build.gradle || -f $git_root/settings.gradle ]]; then
         is_in_context=true
       fi
     fi
