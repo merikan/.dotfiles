786b0776f3216aff0414f946f277cfab041da888
Peter Merikan <peter@merikan.com>
2021-04-28 09:23:11 +0200

    spaceship-prompt: clean up java.zsh

--- spaceship-prompt/sections/java.zsh
+++ spaceship-prompt/sections/java.zsh
@@ -12,22 +12,18 @@ SPACESHIP_JAVA_PREFIX="${SPACESHIP_JAVA_PREFIX="$SPACESHIP_PROMPT_DEFAULT_PREFIX
 SPACESHIP_JAVA_SUFFIX="${SPACESHIP_JAVA_SUFFIX="$SPACESHIP_PROMPT_DEFAULT_SUFFIX"}"
 SPACESHIP_JAVA_SYMBOL="${SPACESHIP_JAVA_SYMBOL="☕ "}"
 SPACESHIP_JAVA_COLOR="${SPACESHIP_JAVA_COLOR="cyan"}"
-# ------------------------------------------------------------------------------
-# Section
-# ------------------------------------------------------------------------------
-spaceship_java() {
-  [[ $SPACESHIP_JAVA_SHOW == false ]] && return
 
-  spaceship::exists java || return
+# ------------------------------------------------------------------------------
+# Utils
+# ------------------------------------------------------------------------------
 
-  # Show Java version only in Java context
-  if [[ $SPACESHIP_JAVA_CONTEXT_ONLY == true ]]; then
+spaceship::java::is_in_context() {
     local is_in_context=false
     
     if [[ -f pom.xml || -f build.gradle || -f settings.gradle || 
-          -n *.(java|jar|war|class)(#qN^/) ]]; then
+          -n *.(java|class|jar|war)(#qN^/) ]]; then
       is_in_context=true
-    elif spaceship::is_git; then # java project?
+    elif spaceship::is_git; then # java project in git repo?
       local git_root=$(git rev-parse --show-toplevel)
       if (cygpath --version) >/dev/null 2>/dev/null; then
         git_root=$(cygpath -u $git_root)
@@ -37,15 +33,25 @@ spaceship_java() {
       fi
     fi
 
-    [[ $is_in_context == false ]] && return
+    [[ $is_in_context == true ]]
 
-  fi
+}
+
+# ------------------------------------------------------------------------------
+# Section
+# ------------------------------------------------------------------------------
+spaceship_java() {
+  [[ $SPACESHIP_JAVA_SHOW == false ]] && return
+
+  spaceship::exists java || return
+
+  [[ $SPACESHIP_JAVA_CONTEXT_ONLY == true ]] && ! spaceship::java::is_in_context && return
 
   local java_version=$(java -version 2>&1 | head -n 1 | awk -F '"' '{print $2}')
 
   spaceship::section \
     "$SPACESHIP_JAVA_COLOR" \
     "$SPACESHIP_JAVA_PREFIX" \
-    "${SPACESHIP_JAVA_SYMBOL}${java_version}" \
+    "${SPACESHIP_JAVA_SYMBOL}v${java_version}" \
     "$SPACESHIP_JAVA_SUFFIX"
 }
