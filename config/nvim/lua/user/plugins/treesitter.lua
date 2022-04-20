899dc0888babb126de1046e37bcf2b09724a00f0
Peter Merikan <peter@merikan.com>
2022-04-20 13:38:21 +0200

    nvim: fix tresitter 'ensure_installed='maintained' will be removed'

--- config/nvim/lua/user/plugins/treesitter.lua
+++ config/nvim/lua/user/plugins/treesitter.lua
@@ -4,7 +4,7 @@ local M = {}
 M.setup = function()
   -- settings
   require'nvim-treesitter.configs'.setup {
-    ensure_installed = "maintained", -- one of "all", "maintained" (parsers with maintainers), or a list of languages
+    ensure_installed = "all",
     sync_install = false, -- install languages synchronously (only applied to `ensure_installed`)
     ignore_install = { "" }, -- List of parsers to ignore installing
     autopairs = {
