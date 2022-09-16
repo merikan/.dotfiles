c3f3d921e0ca998ed9b7fed6d95287e57c3c2eb9
Peter Merikan <peter@merikan.com>
2022-09-16 13:41:59 +0200

    support spaceship_prompt 4.*

--- config/spaceship-prompt/sections/wip.zsh
+++ config/spaceship-prompt/sections/wip.zsh
@@ -1,32 +1,33 @@
-# do we have a wip commit?
-# check if latest commit message starts with the word 'wip'
+# Do we have a wip commit?
+# Check if latest commit message starts with the word 'wip'
 
 # ------------------------------------------------------------------------------
 # Configuration
 # ------------------------------------------------------------------------------
 SPACESHIP_WIP_SHOW="${SPACESHIP_WIP_SHOW:=true}"
+SPACESHIP_WIP_ASYNC="${SPACESHIP_JAVA_ASYNC=true}"
 SPACESHIP_WIP_PREFIX="${SPACESHIP_WIP_PREFIX="$SPACESHIP_PROMPT_DEFAULT_PREFIX"}"
 SPACESHIP_WIP_SUFFIX="${SPACESHIP_WIP_SUFFIX="$SPACESHIP_PROMPT_DEFAULT_SUFFIX"}"
 SPACESHIP_WIP_SYMBOL="${SPACESHIP_WIP_SYMBOL="🚧 "}"
-SPACESHIP_WIP_TEXT="${SPACESHIP_WIP_TEXT="WIP"}"
 SPACESHIP_WIP_COLOR="${SPACESHIP_WIP_COLOR="red"}"
+SPACESHIP_WIP_TEXT="${SPACESHIP_WIP_TEXT="WIP"}"
 
 # ------------------------------------------------------------------------------
 # Section
 # ------------------------------------------------------------------------------
-spaceship_mywip() {
+spaceship_wip() {
   [[ $SPACESHIP_WIP_SHOW == false ]] && return
 
   spaceship::is_git || return
 
-  local wip=$(git log -n 1 --pretty=format:%s 2>/dev/null | grep -q -c -i "^wip" && echo true);
+  local is_wip=$(git log -n 1 --pretty=format:%s 2>/dev/null | grep -q -c -i "^wip" && echo true);
 
-  if [[ $wip == true ]]; then
-    # Display WIP section
-    spaceship::section \
-      "$SPACESHIP_WIP_COLOR" \
-      "$SPACESHIP_WIP_PREFIX" \
-      "${SPACESHIP_WIP_SYMBOL}${SPACESHIP_WIP_TEXT}" \
-      "$SPACESHIP_WIP_SUFFIX"
-  fi
+  [[ -n $is_wip ]] || return
+
+  spaceship::section \
+    --color "$SPACESHIP_WIP_COLOR" \
+    --prefix "$SPACESHIP_WIP_PREFIX" \
+    --suffix "$SPACESHIP_WIP_SUFFIX" \
+    --symbol "${SPACESHIP_WIP_SYMBOL}" \
+    "${SPACESHIP_WIP_TEXT}"
 }
