8a3ba55d7aad2861f0c154a0e9c823acca9d1d94
Peter Merikan <peter@merikan.com>
2023-04-06 08:36:44 +0200

    fix plugin alpha

--- config/nvim/lua/user/plugins/alpha-nvim.lua
+++ config/nvim/lua/user/plugins/alpha-nvim.lua
@@ -1,14 +1,13 @@
 -- https://github.com/goolord/alpha-nvim
 -- https://github.com/goolord/alpha-nvim/discussions/16
 
-local M = {
+return {
   "goolord/alpha-nvim",                           -- a nvim dashboard
-    dependencies = "nvim-tree/nvim-web-devicons"
-}
-
-function M.config()
-  local alpha = require "alpha"
-  local dashboard = require "alpha.themes.dashboard"
+  -- cmd = "Alpha",
+  dependencies = "nvim-tree/nvim-web-devicons",
+  config = function ()
+    local alpha = require "alpha"
+    local dashboard = require "alpha.themes.dashboard"
     dashboard.section.header.val = {
       [[                                                          ]],
       [[                                                          ]],
@@ -22,29 +21,31 @@ function M.config()
       [[ Peter Merikan  (https://merikan.com)                     ]],
       [[ github.com/merikan                                       ]],
     }
-  dashboard.section.buttons.val = {
-    dashboard.button("f", " " .. " Find file", ":Telescope find_files <CR>"),
-    dashboard.button("e", " " .. " New file", ":ene <BAR> startinsert <CR>"),
-    dashboard.button("p", " " .. " Find project", ":lua require('telescope').extensions.projects.projects()<CR>"),
-    dashboard.button("r", " " .. " Recent files", ":Telescope oldfiles <CR>"),
-    dashboard.button("t", " " .. " Find text", ":Telescope live_grep <CR>"),
-    dashboard.button("c", " " .. " Config", ":e $MYVIMRC <CR>"),
-    dashboard.button("q", " " .. " Quit", ":qa<CR>"),
-  }
+    dashboard.section.buttons.val = {
+      dashboard.button("f", " " .. " Find file", ":Telescope find_files <CR>"),
+      dashboard.button("e", " " .. " New file", ":ene <BAR> startinsert <CR>"),
+      dashboard.button("p", " " .. " Find project", ":lua require('telescope').extensions.projects.projects()<CR>"),
+      dashboard.button("r", " " .. " Recent files", ":Telescope oldfiles <CR>"),
+      dashboard.button("t", " " .. " Find text", ":Telescope live_grep <CR>"),
+      dashboard.button("c", " " .. " Config", ":e $MYVIMRC <CR>"),
+      dashboard.button("q", " " .. " Quit", ":qa<CR>"),
+    }
     dashboard.section.footer.val = {
       "                                                                                       ",
       "                                                                                       ",
       "                                                                                       ",
       "                                                                                       ",
-      "In software development, complexity might be admired but simplicity is always rewarded.",
+      "                     In software development,                                          ",
+      "                           complexity might be admired                                 ",
+      "                                 but simplicity is always rewarded.                    ",
     }
+    dashboard.section.footer.opts.hl = "Type"
+    dashboard.section.header.opts.hl = "Include"
+    dashboard.section.buttons.opts.hl = "Keyword"
 
-  dashboard.section.footer.opts.hl = "Type"
-  dashboard.section.header.opts.hl = "Include"
-  dashboard.section.buttons.opts.hl = "Keyword"
-
-  dashboard.opts.opts.noautocmd = true
-  alpha.setup(dashboard.opts)
-end
-
-return M
+    -- dashboard.config.layout[1].val = vim.fn.max { 2, vim.fn.floor(vim.fn.winheight(0) * 0.2) }
+    -- dashboard.config.layout[3].val = 5
+    -- dashboard.config.opts.noautocmd = true
+    alpha.setup(dashboard.opts)
+  end,
+}
