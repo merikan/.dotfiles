dc29bb1f996559397a4894a729b2797d96388dcc
Peter Merikan <peter@merikan.com>
2023-10-04 17:57:12 +0200

    feat(nvim): remove lazy extras.coding.yanky

--- config/nvim/lua/config/lazy.lua
+++ config/nvim/lua/config/lazy.lua
@@ -11,7 +11,6 @@ require("lazy").setup {
     -- add LazyVim and import its plugins
     { "LazyVim/LazyVim", import = "lazyvim.plugins" },
     -- import any extras modules here
-    { import = "lazyvim.plugins.extras.coding.yanky" },
     { import = "lazyvim.plugins.extras.test.core" },
     { import = "lazyvim.plugins.extras.lang.go" },
     { import = "lazyvim.plugins.extras.lang.rust" },
