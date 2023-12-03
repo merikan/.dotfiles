e6dc4cf1535c7061eaf4095f5a7f99a94689d3c7
Peter Merikan <peter@merikan.com>
2023-12-03 08:42:02 +0100

    feat(vim): add extras.editor.aerial

---
+++ config/nvim/lua/plugins/editor_aerial.lua
@@ -0,0 +1,11 @@
+--  plugin for a code outline window
+-- https://github.com/stevearc/aerial.nvim
+return {
+  "stevearc/aerial.nvim",
+  opts = {
+    layout = {
+      default_direction = "prefer_left",
+      resize_to_content = true,
+    },
+  },
+}
