5c63837c9c6a84231c69d1d43f2517a309a12695
Peter Merikan <peter@merikan.com>
2023-12-03 08:32:52 +0100

    feat(vim): add 'allaman/kustomize.nvim' plugin

---
+++ config/nvim/lua/plugins/lang_kustomize.lua
@@ -0,0 +1,8 @@
+-- useful functions for working with Kustomize
+-- https://github.com/Allaman/kustomize.nvim
+return {
+  "allaman/kustomize.nvim",
+  requires = "nvim-lua/plenary.nvim",
+  ft = "yaml",
+  config = true,
+}
