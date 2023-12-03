87f171d6ba96f792b52c05fe3df1a377c126b9eb
Peter Merikan <peter@merikan.com>
2023-12-03 17:58:19 +0100

    feat(vim): add more neotest adapters

--- config/nvim/lua/plugins/neotest.lua
+++ config/nvim/lua/plugins/neotest.lua
@@ -1,7 +1,23 @@
+-- For test runners:
+-- https://github.com/nvim-neotest/neotest#supported-runners
 return {
-  { "nvim-neotest/neotest-plenary" },
   {
     "nvim-neotest/neotest",
-    opts = { adapters = { "neotest-plenary" } },
+    dependencies = {
+      "nvim-neotest/neotest-plenary",
+      "rcasia/neotest-java",
+      "nvim-neotest/neotest-go",
+      "rouge8/neotest-rust",
+      "sidlatau/neotest-dart"
+    },
+    opts = {
+      adapters = {
+        ["neotest-plenary"] = {},
+        ["neotest-java"] = {},
+        ["neotest-go"] = {},
+        ["neotest-rust"] = {},
+        ["neotest-dart"] = {},
+      },
+    },
   },
 }
