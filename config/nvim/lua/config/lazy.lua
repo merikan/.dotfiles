fe898b177e8749883a43900e9ec98c9f53a150eb
Peter Merikan <peter@merikan.com>
2023-12-03 18:13:20 +0100

    feat(nvim): add 'extras.dap.core'

--- config/nvim/lua/config/lazy.lua
+++ config/nvim/lua/config/lazy.lua
@@ -12,6 +12,7 @@ require("lazy").setup {
     { "LazyVim/LazyVim", import = "lazyvim.plugins" },
     -- import any extras modules here
     { import = "lazyvim.plugins.extras.editor.aerial" },
+    { import = "lazyvim.plugins.extras.dap.core" },
     { import = "lazyvim.plugins.extras.test.core" },
     { import = "lazyvim.plugins.extras.lang.json" },
     { import = "lazyvim.plugins.extras.lang.go" },
