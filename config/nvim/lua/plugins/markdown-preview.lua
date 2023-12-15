19239df7a5bbd0724cfe55a66a1464240de3e869
Peter Merikan <peter@merikan.com>
2023-12-15 09:50:26 +0100

    feat(nvim): add ft plantuml to markdown-preview plugin

--- config/nvim/lua/plugins/markdown-preview.lua
+++ config/nvim/lua/plugins/markdown-preview.lua
@@ -1,6 +1,6 @@
 return  {
     "iamcco/markdown-preview.nvim",
-    ft = "markdown",
+    ft = "markdown, plantuml",
     -- build = "cd app && yarn install",
     build = ":call mkdp#util#install()",
   }
