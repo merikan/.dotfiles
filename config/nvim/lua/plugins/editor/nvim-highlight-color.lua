3ebec784cc398b649fc45cd31f6cf624656e6632
Peter Merikan <peter@merikan.com>
2024-03-06 07:47:17 +0100

    feat(nvim): add highlight color plugin

---
+++ config/nvim/lua/plugins/editor/nvim-highlight-color.lua
@@ -0,0 +1,9 @@
+return {
+  "brenoprata10/nvim-highlight-colors",
+  -- enabled = false,
+  opts = {
+    render = "background", -- background | foreground | virtual
+    enable_named_colors = true,
+    enable_tailwind = false,
+  },
+}
