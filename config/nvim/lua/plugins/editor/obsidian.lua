9c031ad3e99ebe1658a6db44eafefd902abd9fda
Peter Merikan <peter@merikan.com>
2024-03-06 07:48:29 +0100

    feat(nvim): polish obsidian

--- config/nvim/lua/plugins/editor/obsidian.lua
+++ config/nvim/lua/plugins/editor/obsidian.lua
@@ -1,7 +1,7 @@
 -- A Neovim plugin for writing and navigating an Obsidian vault
 -- https://github.com/epwalsh/obsidian.nvim
+-- if true then return {} end
 return {
-
   "epwalsh/obsidian.nvim",
   version = "*", -- recommended, use latest release instead of latest commit
   lazy = true,
@@ -20,37 +20,53 @@ return {
       dependencies = "nvim-treesitter/nvim-treesitter",
       config = true,
     },
-    -- see below for full list of optional dependencies 👇
   },
   keys = {
-    { "<leader>oo", "<cmd>ObsidianOpen<cr>" },
-    { "<leader>on", "<cmd>ObsidianNew<cr>" },
-    { "<leader>oT", "<cmd>ObsidianTemplate<cr>" },
-    { "<leader>ot", "<cmd>ObsidianToday<cr>" },
-    { "<leader>oy", "<cmd>ObsidianYesterday<cr>" },
-    { "<leader>ol", "<cmd>ObsidianLink<cr>" },
-    { "<leader>oL", "<cmd>ObsidianLinkNew<cr>" },
-    { "<leader>ob", "<cmd>ObsidianBacklinks<cr>" },
-    { "<leader>os", "<cmd>ObsidianSearch<cr>" },
-    { "<leader>oq", "<cmd>ObsidianQuickSwitch<cr>" },
+    { "<leader>oo", "<cmd>ObsidianOpen<CR>", desc = "Open in App" },
+    { "<leader>og", "<cmd>ObsidianSearch<CR>", desc = "Grep" },
+    { "<leader>sO", "<cmd>ObsidianSearch<CR>", desc = "Obsidian Grep" },
+    { "<leader>on", "<cmd>ObsidianNew<CR>", desc = "New Note" },
+    { "<leader>o<space>", "<cmd>ObsidianQuickSwitch<CR>", desc = "Find Files" },
+    { "<leader>fo", "<cmd>ObsidianQuickSwitch<CR>", desc = "Find Obsidian Files" },
+    { "<leader>ob", "<cmd>ObsidianBacklinks<CR>", desc = "Backlinks" },
+    { "<leader>ot", "<cmd>ObsidianToday<cr>", desc = "Today note" },
+    { "<leader>oy", "<cmd>ObsidianYesterday<cr>", desc = "tomorrow note" },
+    { "<leader>o#", "<cmd>ObsidianTags<CR>", desc = "Tags" },
+    { "<leader>oT", "<cmd>ObsidianTemplate<CR>", desc = "Template" },
+    { "<leader>ol", "<cmd>ObsidianLink<CR>", desc = "Link" },
+    { "<leader>oL", "<cmd>ObsidianLinks<CR>", desc = "Links" },
+    { "<leader>oN", "<cmd>ObsidianLinkNew<CR>", desc = "New Link" },
+    { "<leader>oe", "<cmd>ObsidianExtractNote<CR>", desc = "Extract Note" },
+    { "<leader>ow", "<cmd>ObsidianWorkspace<CR>", desc = "Workspace" },
+    { "<leader>or", "<cmd>ObsidianRename<CR>", desc = "Rename" },
+    { "<leader>oi", "<cmd>ObsidianPasteImg<CR>", desc = "Paste Image" },
   },
   opts = {
     workspaces = {
       {
         name = "personal",
-        path = "~/Documents/obsidian",
+        path = "~/Notes",
       },
     },
     templates = {
       subdir = "Templates",
       date_format = "%Y-%m-%d-%a",
       time_format = "%H:%M",
+      substitutions = {
+        yesterday = function()
+          return os.date("%Y-%m-%d", os.time() - 86400)
+        end,
+        tomorrow = function()
+          return os.date("%Y-%m-%d", os.time() + 86400)
+        end,
+      },
     },
-    note_id_func = function(title) return tostring(title) end,
+    note_id_func = function(title)
+      return tostring(title)
+    end,
   },
   config = function(_, opts)
     require("obsidian").setup(opts)
-
     -- if cursor is on a link in an obsidian file, gf will follow the reference, otherwise it will behave normally
     vim.keymap.set("n", "gf", function()
       if require("obsidian").util.cursor_on_markdown_link() then
@@ -64,7 +80,9 @@ return {
 -- add menu
 {
   "folke/which-key.nvim",
-  opts = function(_, opts)
-    opts.defaults["<leader>o"] = { name = "+obsidian" }
-  end,
+  opts = {
+    defaults = {
+      ["<leader>o"] = { name = "obsidian" },
+    },
+  },
 }
