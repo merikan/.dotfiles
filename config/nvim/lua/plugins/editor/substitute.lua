6e1eb8e7f43264c4ad664bb8955cee24b483a80e
Peter Merikan <peter@merikan.com>
2024-03-17 19:41:33 +0100

    feat(nvim): add substitute.nvim plugin

---
+++ config/nvim/lua/plugins/editor/substitute.lua
@@ -0,0 +1,14 @@
+return {
+  "gbprod/substitute.nvim",
+  keys = {
+    { mode = "n", "gr", "<cmd>lua require('substitute').operator()<cr>", desc = "substitute" },
+    { mode = "n", "grr", "<cmd>lua require('substitute').line()<cr>", desc = "substitute-line" },
+    { mode = "n", "gR", "<cmd>lua require('substitute').eol()<cr>", desc = "substitute-eol" },
+    { mode = "x", "gr", "<cmd>lua require('substitute').visual()<cr>", desc = "substitute" },
+  },
+  opts = {
+    -- your configuration comes here
+    -- or leave it empty to use the default settings
+    -- refer to the configuration section below
+  },
+}
