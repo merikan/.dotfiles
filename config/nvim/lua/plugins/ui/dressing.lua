3a08903170240618e2cf00a24244faf789aa4cfa
Peter Merikan <peter@merikan.com>
2024-03-17 19:43:23 +0100

    feat(nvim): use vim motions in modal (dressing)

---
+++ config/nvim/lua/plugins/ui/dressing.lua
@@ -0,0 +1,9 @@
+return   {
+    "dressing.nvim",
+    opts = {
+      input = {
+        -- When true, <Esc> will close the modal - Defaults to true
+        insert_only = false,  -- vim motion in modal
+      },
+    },
+  }
