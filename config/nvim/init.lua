603ef6b2028ed8847edcfc62244ac47883debfcf
Peter Merikan <peter@merikan.com>
2024-05-28 19:36:19 +0200

    style(nvim): polish

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -21,15 +21,10 @@
 -- My config now uses the amazing LazyVim as boilerplate, because I'm a lazy developer ;-)
 -- https://www.lazyvim.org/
 
+-- vim.lsp.set_log_level('debug')
+
 require("util.globals")
 require("util.autocmds")
 -- bootstrap LazyVim, lazy.nvim and plugins
 require("config.lazy")    -- load lazyVim, because I'm lazy
 
--- require "user.core.globals"         -- load global functions
--- require "user.core.keymaps"         -- load keybindings
--- require "user.core.autocommands"    -- register autocommands
--- require "user.core.options"         -- load settings
--- require "user.core.plugins"         -- load plugins
--- require "user.lsp"                  -- configure lsp
--- require "user.core.colorscheme"     -- set color scheme
