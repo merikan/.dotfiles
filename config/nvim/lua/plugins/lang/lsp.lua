ccb7fbf0c77bf0987840879e0ed8287fd4ead95b
Peter Merikan <peter@merikan.com>
2024-07-12 08:54:05 +0200

    feat(nvim): activate tiny-inline-diagnostics plugin

--- config/nvim/lua/plugins/lang/lsp.lua
+++ config/nvim/lua/plugins/lang/lsp.lua
@@ -1,9 +1,19 @@
-return
+return {
   {
-  -- plugin for previewing native LSP's goto definition, type definition, implementation, declaration and references calls in floating windows.
-  -- https://github.com/rmagatti/goto-preview
-  'rmagatti/goto-preview',
-  config = function()
-    require('goto-preview').setup {}
-  end
+    "nvim-lspconfig",
+    opts = {
+      diagnostics = {
+        virtual_text = false,
+      },
+    },
+  },
+  {
+    -- plugin for previewing native LSP's goto definition, type definition, implementation, declaration and references calls in floating windows.
+    -- https://github.com/rmagatti/goto-preview
+    'rmagatti/goto-preview',
+    config = function()
+      require('goto-preview').setup {}
+    end
+  }
 }
+
