cc9250d725750d2cab35b7b8f382c4614dfa8806
Peter Merikan <peter@merikan.com>
2024-07-19 09:56:14 +0200

    feat(kitty,nvim): open kitty scrollback in nvim

---
+++ config/nvim/lua/plugins/misc/kitty-scrollback-nvim.lua
@@ -0,0 +1,11 @@
+return  {
+    'mikesmithgh/kitty-scrollback.nvim',
+    enabled = true,
+    lazy = true,
+    cmd = { 'KittyScrollbackGenerateKittens', 'KittyScrollbackCheckHealth' },
+    event = { 'User KittyScrollbackLaunch' },
+    version = '*',
+    config = function()
+      require('kitty-scrollback').setup()
+    end,
+  }
