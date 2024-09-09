d0067ccb5518f8cea21b87d488e4d122b2e7af45
Peter Merikan <peter@merikan.com>
2024-09-09 15:59:04 +0200

    refactor(ncim): use yazi.nvim instead of tfm.nvim

---
+++ config/nvim/lua/plugins/editor/yazi-nvim.lua
@@ -0,0 +1,17 @@
+-- yazi terminal file manager
+-- https://github.com/mikavilpas/yazi.nvim
+return
+{
+  "mikavilpas/yazi.nvim",
+  event = "VeryLazy",
+  keys = {
+    { "<M-o>", "<cmd>Yazi<CR>", desc = "Open file in File Manager" },
+    { "<leader>fm", "<cmd>Yazi cwd<CR>", desc = "File Manager" },
+  },
+  opts = {
+    open_for_directories = false,
+    keymaps = {
+      show_help = '<f1>',
+    },
+  },
+}
