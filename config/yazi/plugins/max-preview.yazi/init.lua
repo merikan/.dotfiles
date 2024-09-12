b16b76fe8f60112d1580c2fcf5542dea276624ea
Peter Merikan <peter@merikan.com>
2024-09-12 13:35:45 +0200

    fix(yazi): update max-preview plugin due to #1257

--- config/yazi/plugins/max-preview.yazi/init.lua
+++ config/yazi/plugins/max-preview.yazi/init.lua
@@ -1,22 +1,20 @@
 local function entry(st)
-  if st.old then
-    Manager.layout, st.old = st.old, nil
-  else
-    st.old = Manager.layout
-    Manager.layout = function(self, area)
-      self.area = area
-
-      return ui.Layout()
-        :direction(ui.Layout.HORIZONTAL)
-        :constraints({
-          ui.Constraint.Percentage(0),
-          ui.Constraint.Percentage(0),
-          ui.Constraint.Percentage(100),
-        })
-        :split(area)
-    end
-  end
-  ya.app_emit("resize", {})
+	if st.old then
+		Tab.layout, st.old = st.old, nil
+	else
+		st.old = Tab.layout
+		Tab.layout = function(self)
+			self._chunks = ui.Layout()
+				:direction(ui.Layout.HORIZONTAL)
+				:constraints({
+					ui.Constraint.Percentage(0),
+					ui.Constraint.Percentage(0),
+					ui.Constraint.Percentage(100),
+				})
+				:split(self._area)
+		end
+	end
+	ya.app_emit("resize", {})
 end
 
 return { entry = entry }
