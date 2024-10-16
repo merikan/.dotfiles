5a81010defa59517db5aaa7aec0f66b4e06ed8e2
Peter Merikan <peter@merikan.com>
2024-10-16 21:18:26 +0200

    feat(wezterm): right alt should be option on macos

--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -11,9 +11,7 @@ config.font_size = 14.0
 config.color_scheme = 'Gruvbox dark, soft (base16)'
 
 config.hide_tab_bar_if_only_one_tab = true
--- left-alt is option when macos
-config.send_composed_key_when_left_alt_is_pressed = true
--- right-alt is always alt
-config.send_composed_key_when_right_alt_is_pressed = false
+config.send_composed_key_when_left_alt_is_pressed = false
+config.send_composed_key_when_right_alt_is_pressed = true
 
 return config
