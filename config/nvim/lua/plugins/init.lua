edaae6aba58b6fc8714fcf1f8fab38222cdc1f6e
Peter Merikan <peter@merikan.com>
2024-10-16 21:02:21 +0200

    feat(nvim): use java extras

--- config/nvim/lua/plugins/init.lua
+++ config/nvim/lua/plugins/init.lua
@@ -13,6 +13,7 @@ return {
   { import = "lazyvim.plugins.extras.editor.outline" },
   { import = "lazyvim.plugins.extras.lang.go" },
   { import = "lazyvim.plugins.extras.lang.json" },
+  { import = "lazyvim.plugins.extras.lang.java" },
   { import = "lazyvim.plugins.extras.lang.markdown" },
   { import = "lazyvim.plugins.extras.lang.nix" },
   { import = "lazyvim.plugins.extras.lang.rust" },
--- config/nvim/lua/plugins/lang/java.lua
+++ config/nvim/lua/plugins/lang/java.lua
@@ -1,63 +1,31 @@
 return {
+  -- must of the things are wired up with Java Extra
+  -- https://www.lazyvim.org/extras/lang/java
   {
     "williamboman/mason.nvim",
     opts = function(_, opts)
       opts.ensure_installed = opts.ensure_installed or {}
       vim.list_extend(opts.ensure_installed, {
-        "jdtls",
-        "java-debug-adapter",
-        "java-test",
         "google-java-format",
         "checkstyle",
       })
     end,
   },
-  {
-    "nvim-treesitter/nvim-treesitter",
-    opts = {
-      ensure_installed = {
-        "java",
-      },
-    },
-  },
-  {
-    "neovim/nvim-lspconfig",
-    dependencies = { "mfussenegger/nvim-jdtls" },
-    opts = {
-      setup = {
-        jdtls = function()
-          return true -- don't load with nvim-lspconfig since we use ftplugin
-        end,
-      },
-    },
-  },
-  {
-    "nvim-neotest/neotest",
-    optional = true,
-    dependencies = {
-      "rcasia/neotest-java",
-    },
-    opts = {
-      adapters = {
-        ["neotest-java"] = {},
-      },
-    },
-  },
-  {
-    "stevearc/conform.nvim",
-    optional = true,
-    opts = {
-      formatters_by_ft = {
-        -- java = { "google-java-format" },
-      },
-    },
-  },
-  {
-    "mfussenegger/nvim-lint",
-    opts = {
-      linters_by_ft = {
-        -- java = { "checkstyle" },
-      },
-    },
-  },
+  -- {
+  --   "stevearc/conform.nvim",
+  --   optional = true,
+  --   opts = {
+  --     formatters_by_ft = {
+  --       -- java = { "google-java-format" },
+  --     },
+  --   },
+  -- },
+  -- {
+  --   "mfussenegger/nvim-lint",
+  --   opts = {
+  --     linters_by_ft = {
+  --       -- java = { "checkstyle" },
+  --     },
+  --   },
+  -- },
 }
