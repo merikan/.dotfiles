d2cec8d317ddd87aa3bb0d3d79df1f60316fa8c1
Peter Merikan <peter@merikan.com>
2025-01-15 12:40:42 +0100

    fix(yazi): fix deprecated async #1966

--- config/yazi/plugins.local/parent-arrow.yazi/init.lua
+++ config/yazi/plugins.local/parent-arrow.yazi/init.lua
@@ -1,18 +1,12 @@
-local function entry(_, args)
+--- https://yazi-rs.github.io/docs/tips/#parent-arrow
+--- @sync entry
+local function entry(_, job)
 	local parent = cx.active.parent
 	if not parent then return end
 
-	local offset = tonumber(args[1])
-	if not offset then return ya.err(args[1], 'is not a number') end
-
-	local start = parent.cursor + 1 + offset
-	local end_ = offset < 0 and 1 or #parent.files
-	local step = offset < 0 and -1 or 1
-	for i = start, end_, step do
-		local target = parent.files[i]
-		if target and target.cha.is_dir then
-			return ya.manager_emit("cd", { target.url })
-		end
+	local target = parent.files[parent.cursor + 1 + job.args[1]]
+	if target and target.cha.is_dir then
+		ya.manager_emit("cd", { target.url })
 	end
 end
 
