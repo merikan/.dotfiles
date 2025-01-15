f02789353210613440d123661b0740f39de590bc
Peter Merikan <peter@merikan.com>
2025-01-15 08:09:29 +0100

    refactor(nvim): move lazyvim extras to json file

--- config/nvim/lua/plugins/init.lua
+++ config/nvim/lua/plugins/init.lua
@@ -4,20 +4,7 @@
 -- for examples how to config https://www.lazyvim.org/configuration/examples
 
 return {
-  -- -- import any extras modules here
-  -- see ../../lazyvim.json for added extras
 
-  { import = "lazyvim.plugins.extras.coding.mini-surround" },
-  { import = "lazyvim.plugins.extras.dap.core" },
-  { import = "lazyvim.plugins.extras.editor.outline" },
-  { import = "lazyvim.plugins.extras.lang.go" },
-  { import = "lazyvim.plugins.extras.lang.json" },
-  { import = "lazyvim.plugins.extras.lang.java" },
-  { import = "lazyvim.plugins.extras.lang.markdown" },
-  { import = "lazyvim.plugins.extras.lang.nix" },
-  { import = "lazyvim.plugins.extras.lang.rust" },
-  { import = "lazyvim.plugins.extras.lang.toml" },
-  { import = "lazyvim.plugins.extras.test.core" },
   --
   { import = "plugins.coding" },
   { import = "plugins.editor" },
