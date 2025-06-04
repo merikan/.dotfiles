607196149bbcee081c9e0ac1b602584d95b09431
Peter Merikan <peter@merikan.com>
2025-06-04 08:07:22 +0200

    feat(nvim): add checkmate, a todo plugin

---
+++ config/nvim/lua/plugins/misc/check-mate.lua
@@ -0,0 +1,12 @@
+-- A simple yet sophisticated Neovim To-do plugin
+-- https://github.com/bngarren/checkmate.nvim
+
+return {
+    "bngarren/checkmate.nvim",
+    ft = "markdown", -- Lazy loads for Markdown files matching patterns in 'files'
+    opts = {
+        -- your configuration here
+        -- or leave empty to use defaults
+    },
+
+}
