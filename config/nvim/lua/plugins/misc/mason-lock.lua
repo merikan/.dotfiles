3d62cc9a1a6ce7deee6bd9b35cdee5a15b611993
Peter Merikan <peter@merikan.com>
2025-07-28 14:09:47 +0200

    fix(nvim): lock mason-lock to a commit

--- config/nvim/lua/plugins/misc/mason-lock.lua
+++ config/nvim/lua/plugins/misc/mason-lock.lua
@@ -4,6 +4,7 @@ return {
   "williamboman/mason.nvim",
   dependencies = {
     "zapling/mason-lock.nvim",
+    commit = "5cb1be532b12d586ee001a9a07aa900fcb090e1c",
     cmd = { "MasonLock", "MasonLockRestore" },
     opts = {},
   },
