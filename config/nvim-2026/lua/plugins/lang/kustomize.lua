17e852e2b64bdceafffd0869b4f21b87230ed65c
Peter Merikan <peter@merikan.com>
2025-12-21 10:21:59 +0100

    feat(nvim): add kustomize

---
+++ config/nvim-2026/lua/plugins/lang/kustomize.lua
@@ -0,0 +1,10 @@
+return {
+  -- useful functions for working with Kustomize
+  -- https://github.com/Allaman/kustomize.nvim
+  {
+    "allaman/kustomize.nvim",
+    requires = "nvim-lua/plenary.nvim",
+    ft = "yaml",
+    config = true,
+  },
+}
