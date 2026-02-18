c330f967a6362be50cd71e762febd2961846a3da
Peter Merikan <peter@merikan.com>
2026-02-18 13:00:44 +0100

    feat(nvim); add code-diff

---
+++ config/nvim-2026/lua/plugins/coding/code-diff.lua
@@ -0,0 +1,7 @@
+-- side-by-side diff rendering with two-tier highlighting
+-- https://github.com/esmuellert/codediff.nvim
+return {
+  "esmuellert/codediff.nvim",
+  dependencies = { "MunifTanjim/nui.nvim" },
+  cmd = "CodeDiff",
+}
