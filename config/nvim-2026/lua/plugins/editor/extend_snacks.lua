bbefbf8373651bd5a4830853d994d3906675dce4
Peter Merikan <peter@merikan.com>
2026-07-17 10:40:55 +0200

    feat(nvim-2026): avoid core-dump in snacks

--- config/nvim-2026/lua/plugins/editor/extend_snacks.lua
+++ config/nvim-2026/lua/plugins/editor/extend_snacks.lua
@@ -1,6 +1,14 @@
 return {
+   -- dir = "/Users/peter/Repos/github.com/folke/snacks.nvim",
   "folke/snacks.nvim",
   opts = {
+    styles = {
+      notification = {
+        -- prevent core dump
+        --  https://www.reddit.com/r/neovim/comments/1r8eo6b/comment/o64mh28
+        ft = "",
+      },
+    },
     picker = {
       sources = {
         explorer = {
