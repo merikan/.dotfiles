91abacee908946694d7ea48e9b03dcefe3472d00
Peter Merikan <peter@merikan.com>
2026-07-26 09:01:34 +0200

    feat(nvim-2026): add colorizer plugin

---
+++ config/nvim-2026/lua/plugins/editor/colors.lua
@@ -0,0 +1,36 @@
+return {
+  -- https://github.com/catgoose/nvim-colorizer.lua
+  {
+    "catgoose/nvim-colorizer.lua",
+    event = "BufReadPre",
+    opts = { -- set to setup table
+    },
+  },
+  -- {
+  --   "brenoprata10/nvim-highlight-colors",
+  --   enabled = false,
+  --   opts = {
+  --     render = "background", -- background | foreground | virtual
+  --     enable_named_colors = true,
+  --     enable_tailwind = false,
+  --   },
+  -- },
+  -- {
+  --   -- Color picker and highlighter plugin for Neovim.
+  --   -- https://github.com/uga-rosa/ccc.nvim/tree/main
+  --   "uga-rosa/ccc.nvim",
+  --   ft = { "html", "javascript", "typescript", "javascriptreact", "typescriptreact", "css", "scss", "sh" },
+  --   cmd = { "CccPick", "CccConvert", "CccHighlighterEnable", "CccHighlighterDisable", "CccHighlighterToggle" },
+  --   keys = {
+  --     { "<leader>u+c", "<cmd>CccHighlighterToggle<cr>", desc = "Toggle Paint/Colorizer" },
+  --     { "<A-C>", "<cmd>CccConvert<cr>", desc = "Convert color" },
+  --     { "<A-c>", "<cmd>CccPick<cr>", desc = "Pick Color" },
+  --   },
+  --   opts = {
+  --     highlighter = {
+  --       auto_enable = true,
+  --       lsp = true,
+  --     },
+  --   },
+  -- },
+}
